48bf920de7af2967e75a33bad82401aa8088f589
Alper Simsek <alper.simsek@trendyol.com>
2019-03-07 13:24:29 +0300

    remomve

--- main.go
+++ main.go
@@ -10,41 +10,41 @@ import (
 
 // Article - Our struct for all articles
 type UserInput struct {
-	id      string    `json:"id"`
-	timestamp   string    `json:"timmestamp"`
-	Desc    string    `json:"desc"`
-	Content string    `json:"content"`
+	ID        string `json:"id"`
+	Timestamp string `json:"timestamp"`
+	Input     string `json:"input"`
+	Content   string `json:"content"`
 }
 
-type Articles []Article
+type UserInputs []UserInput
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+func returnAllUserInputs(w http.ResponseWriter, r *http.Request) {
+	inputs := UserInputs{
+		UserInput{ID: "123", Timestamp: "12:23:22", Input: "cheese", Content: "meal"},
+		UserInput{ID: "456", Timestamp: "12:23:25", Input: "chicken", Content: "meal"},
 	}
-	fmt.Println("Endpoint Hit: returnAllArticles")
+	fmt.Println("Endpoint Hit: returnAllUserInputs")
 
-	json.NewEncoder(w).Encode(articles)
+	json.NewEncoder(w).Encode(inputs)
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+func returnSingleInput(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	key := vars["id"]
+	key := vars["ID"]
 	fmt.Fprintf(w, "Key: "+key)
 }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle).Methods("GET")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle).Methods("POST")
+	myRouter.HandleFunc("/all", returnAllUserInputs)
+	myRouter.HandleFunc("/article/{ID}", returnSingleInput).Methods("GET")
+	myRouter.HandleFunc("/article/{ID}", returnSingleInput).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
